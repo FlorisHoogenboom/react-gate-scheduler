f2f1531eab0b72b75d16c98a1e6858fd21070bf7
Floris Hoogenboom <floris@digitaldreamworks.nl>
2022-10-19 20:59:24 +0200

    Add dead button for viewing watchlist.

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import {DndProvider} from 'react-dnd';
 import {ThemeProvider} from './theming';
 import GateChart from './GateChart';
 import DropArea from './DropArea';
+import {FormControl, FormControlLabel, FormGroup, FormLabel, Switch} from "@mui/material";
 
 function App() {
     const [time, setTime] = useState(
@@ -32,12 +33,23 @@ function App() {
     return (
         <ThemeProvider>
             <DndProvider backend={HTML5Backend}>
-                <div style={{display: 'flex'}}>
-                    <div style={{width: '100%'}}>
-                        <div style={{width: '100%', height: '100px', padding: '20px', boxSizing: 'border-box'}}>
+                <div>
+                    <div style={{borderBottom: '10px solid #000000'}}>
+                        <FormControl component="fieldset">
+                            <FormGroup aria-label="position" row>
+                                <FormControlLabel
+                                    value="start"
+                                    control={<Switch color="primary" />}
+                                    label="Show watchlist only"
+                                    labelPlacement="start"
+                                />
+                            </FormGroup>
+                        </FormControl>
+                    </div>
+                    <div>
+                        <div style={{height: '100px', padding: '20px', boxSizing: 'border-box'}}>
                             <DropArea
                                 text="Add to watchlist"></DropArea>
-
                         </div>
                         <GateChart
                             startTime={time}
